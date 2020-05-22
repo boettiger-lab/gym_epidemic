d7fb1f818d82e63867661f4d1672678e192f40a2
mlap <marcuslapeyrolerie@me.com>
2020-05-22 18:40:20 +0000

    Now working with sb3!

--- sir_examples/stable_baselines/sb_sac_v3.py
+++ sir_examples/stable_baselines/sb_sac_v3.py
@@ -3,21 +3,33 @@ import sys
 sys.path.append(os.path.realpath('../..'))
 
 import gym
+from gym import spaces
 import sir_gym
 import numpy as np
 
-from stable_baselines.sac.policies import MlpPolicy, FeedForwardPolicy
-from stable_baselines import SAC
+import torch as th
 
-class CustomSACPolicy(FeedForwardPolicy):
-    def __init__(self, *args, **kwargs):
-        super(CustomSACPolicy, self).__init__(*args, **kwargs,
-                                           layers=[400, 400, 400, 400, 400],
-                                           layer_norm=False,
-                                           feature_extraction="mlp")
+from stable_baselines3.sac.policies import SACPolicy
+from stable_baselines3 import SAC
 
-env = gym.make('sir-v3', intervention='fs', random_params=True, random_obs=True)
+class ConstantSchedule():
+    def __init__(self, value):
+        self._value = value
 
-model = SAC(CustomSACPolicy, env, verbose=1, batch_size=512, buffer_size=100000, learning_starts=10000, ent_coef=0.01, learning_rate=0.0001)
+    def value(self, step):
+        return self._value
+    
+    def __call__(self, value):
+        return self._value
+
+env = gym.make('sir-v3', intervention='fs', random_params=True, random_obs=False)
+
+policy = SACPolicy(net_arch=[400,400,400,400,400],
+                    observation_space = spaces.Box(low=0, high=10**2, shape=(4,), dtype=np.float64),
+                    action_space = spaces.Box(low=0, high=1, shape=(1,), dtype=np.float64),
+                    lr_schedule = ConstantSchedule(0.0001))
+
+policy_kwargs = dict(activation_fn=th.nn.ReLU, net_arch=[400, 400, 400, 400, 400])
+model = SAC("MlpPolicy", env, verbose=1, batch_size=512, buffer_size=100000,  policy_kwargs=policy_kwargs, learning_starts=10000, ent_coef=0.01)
 model.learn(total_timesteps=int(1e5), log_interval=1000)
 model.save("sir_sac_v3_fs.zip")
