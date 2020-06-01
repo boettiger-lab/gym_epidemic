88e199060351a283289dada231d0371d02960b27
mlap <marcuslapeyrolerie@me.com>
2020-06-01 16:12:03 +0000

    Couple rounds of training later

--- sir_examples/stable_baselines/sb_sac_v3.py
+++ sir_examples/stable_baselines/sb_sac_v3.py
@@ -11,10 +11,28 @@ import torch as th
 
 from stable_baselines3.sac.policies import SACPolicy
 from stable_baselines3 import SAC
+output_string = ""
+for i in range(5):
+    env = gym.make('sir-v3', intervention='fs', random_params=True, random_obs=True)
+    policy_kwargs = dict(activation_fn=th.nn.ReLU, net_arch=[256, 256])
+    model = SAC("MlpPolicy", env, verbose=2, batch_size=256, buffer_size=int(1e6), policy_kwargs=policy_kwargs, learning_starts=int(1e4), ent_coef="auto")
+    model.learn(total_timesteps=int(1e5), log_interval=1000)
+    model.save(f"sb3_sac_auto_ent_{i}")
 
-env = gym.make('sir-v3', intervention='fs', random_params=True, random_obs=False)
-
-policy_kwargs = dict(activation_fn=th.nn.ReLU, net_arch=[400, 400, 400, 400, 400])
-model = SAC("MlpPolicy", env, verbose=2, batch_size=512, buffer_size=100000,  policy_kwargs=policy_kwargs, learning_starts=10000, ent_coef=0.01, tensorboard_log="./sac_v3_tb/")
-model.learn(total_timesteps=int(1e5), log_interval=1000)
-model.save("sir_sac_v3_fs.zip")
+    rewards = []
+    for i in range(10):
+        for R0 in np.linspace(2, 10, 50):
+            env = gym.make('sir-v3', intervention='fs')
+            env.covid_sir.random_obs = False 
+            env.covid_sir.random_params = False
+            env.covid_sir.R0 = R0
+            obs = env.reset()
+            action, states = model.predict(obs)
+            obs, reward, dones, info = env.step(action)
+            rewards.append(reward)
+    
+    output_string += f"For iteration {i}, avg reward over range of R0: {np.mean(rewards)} \n"
+    del model
+print(output_string)
+print(max_scale)
+    
