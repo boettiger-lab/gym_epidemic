43b0b85befa97cc177caa597fe166adedd3c0082
Marcus Francois Lapeyrolerie <mlapeyro@ln003.brc>
2020-06-04 05:54:26 -0700

    Adding model from 1e7 timestep run

--- sir_examples/stable_baselines/job0.sh
+++ sir_examples/stable_baselines/job0.sh
@@ -6,7 +6,7 @@
 #SBATCH --account=fc_cboettig
 #
 # Partition:
-#SBATCH --partition=savio2_gpu
+#SBATCH --partition=savio2_1080ti
 #SBATCH --qos=savio_normal
 # Other info:
 #SBATCH --nodes=1
@@ -15,17 +15,18 @@
 #SBATCH --gres=gpu:1
 
 # Wall clock limit:
-#SBATCH --time=01:00:00
+#SBATCH --time=00:25:00
 #
 ## Command(s) to run:
 module load python/3.7
 module load cuda/10.1
 module load gcc/8.3.0
-curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-python get-pip.py
-rm get-pip.py
-pip install --upgrade pip
-pip install stable_baselines3
-pip install gym
+# curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+# python get-pip.py
+# rm get-pip.py
+# pip install --upgrade pip
+# pip install stable_baselines3
+# pip install gym
+source ~/.virtualenv/open_ai_covid19/bin/activate
 python sb_sac_v3.py
 
