c158e2c011be1544ac3ad14c42a2b40cefca367a
mlap <mlapeyro@berkeley.edu>
2020-06-10 08:36:58 -0400

    Cleaning up

--- sir_gym/envs/.ipynb_checkpoints/__init__-checkpoint.py
+++
@@ -1,6 +0,0 @@
-from sir_gym.envs.sir_env import SIREnv
-from sir_gym.envs.sir_env_morris_0 import SIREnvMorris
-from sir_gym.envs.sir_env_morris_1 import SIREnvMorris1
-from sir_gym.envs.sir_env_morris_2 import SIREnvMorris2
-from sir_gym.envs.InterventionSIR import *
-from sir_gym.envs.parameters import *
