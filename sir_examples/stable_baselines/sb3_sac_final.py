5054a50ec63a24a0f3d3789bc88fc65242e74355
mlap <marcuslapeyrolerie@me.com>
2020-09-08 16:37:31 +0000

    Latest changes

--- sir_examples/stable_baselines/sb3_sac_final.py
+++ sir_examples/stable_baselines/sb3_sac_final.py
@@ -14,16 +14,16 @@ from stable_baselines3 import SAC
 from stable_baselines3.common.noise import NormalActionNoise 
 output_string = ""
 for i in range(1):
-    env = gym.make('sir-v3', intervention='fc', random_params=True, random_obs=True)
-    policy_kwargs = dict(activation_fn=th.nn.ReLU, net_arch=[64, 64], log_std_init=-1.15)
-    model = SAC("MlpPolicy", env, verbose=2, batch_size=16, learning_rate=6e-4, train_freq=64, tau=.005, buffer_size=int(1e6), policy_kwargs=policy_kwargs, ent_coef=0.005, POI_R0s=[2,3,4], epsilon=1, burn_in=int(0))
-    model.learn(total_timesteps=int(2e6), log_interval=int(5e4))
-    model.save(f"sb3_sac_fc")
+    env = gym.make('sir-v3', intervention='o', random_params=True, random_obs=True)
+    policy_kwargs = dict(activation_fn=th.nn.ReLU, net_arch=[100, 100, 100, 100, 100], log_std_init=-.59)
+    model = SAC("MlpPolicy", env, verbose=2, batch_size=16, learning_rate=5e-5, train_freq=16, tau=.02, buffer_size=int(1e4), policy_kwargs=policy_kwargs, ent_coef=0.005, POI_R0s=[2,3,4], epsilon=1, burn_in=int(0))
+    model.learn(total_timesteps=int(1e7), log_interval=int(5e4))
+    model.save(f"sb3_sac_o_final_1e7")
 
     rewards = []
     for i in range(10):
         for R0 in np.linspace(2, 10, 50):
-            env = gym.make('sir-v3', intervention='fs')
+            env = gym.make('sir-v3', intervention='o')
             env.covid_sir.random_obs = False 
             env.covid_sir.random_params = False
             env.covid_sir.R0 = R0
