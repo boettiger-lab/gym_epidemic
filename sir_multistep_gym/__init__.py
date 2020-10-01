9f65ba59dac8612aa65d6986c28e298ace8f8e05
mlap <marcuslapeyrolerie@me.com>
2020-10-01 15:27:04 +0000

    Overhaul.

--- sir_multistep_gym/__init__.py
+++
@@ -1,7 +0,0 @@
-from gym.envs.registration import register
-
-register(
-    id='sir-v0',
-    entry_point='sir_multistep_gym.envs:SIREnvMorris2',
-)
-
--- sir_multistep_gym/envs/__init__.py
+++
@@ -1,3 +0,0 @@
-from sir_multistep_gym.envs.sir_env_morris_2 import SIREnvMorris2
-from sir_multistep_gym.envs.InterventionSIR import *
-from sir_multistep_gym.envs.parameters import *
