8c10342f53d6f153fb39b63b4f9118d3da92663f
mlap <marcuslapeyrolerie@me.com>
2020-10-02 16:19:06 +0000

    Adding files for pypi release

--- setup.py
+++ setup.py
@@ -1,10 +1,29 @@
-from setuptools import setup, find_packages
+from distutils.core import setup
 
 setup(
-    name="gym_epidemic",
-    version="0.0.0",
-    description="Provide gym environments for reinforcement learning",
-    packages=find_packages(),
-    python_requires='>=3.6',
+  name = 'gym_epidemic',        
+  packages = ['gym_epidemic'],   
+  version = '0.0.0',
+  license='BSD-3',  
+  description = 'An OpenAI Gym to benchmark AI Reinforcement Learning algorithms in epidemic control problems',  
+  author = 'Marcus Lapeyrolerie',                 
+  author_email = 'marcuslapeyrolerie@me.com',      
+  url = 'https://github.com/boettiger-lab/gym_epidemic',   
+  download_url = 'https://github.com/boettiger-lab/gym_epidemic/releases/tag/v0.0.0',
+  keywords = ['Reinforcement Learning', 'Epidemic Control', 'Epidemics', 
+              "COVID-19", "AI", "stable-baselines", "OpenAI Gym", 
+              "Artificial Intelligence", "Epidemiology"],
+  install_requires=[ 
+          'stable_baselines3',
+          'gym',
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',    
+    'Intended Audience :: Developers', 
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: BSD-3 License',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+  ],
 )
-
