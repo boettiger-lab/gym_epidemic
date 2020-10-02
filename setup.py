7b70c74439c83f7efc4a29a81790fdf756bdd1b4
mlap <marcuslapeyrolerie@me.com>
2020-10-02 16:30:58 +0000

    Small edit to setup

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     'Development Status :: 3 - Alpha',    
     'Intended Audience :: Developers', 
     'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: BSD-3 License',
+    'License :: OSI Approved :: BSD License',
     'Programming Language :: Python :: 3.6',
     'Programming Language :: Python :: 3.7',
     'Programming Language :: Python :: 3.8',
