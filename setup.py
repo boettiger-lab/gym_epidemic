f2282c1e7d69499b7054b4300daeb0289e03f337
mlap <marcuslapeyrolerie@me.com>
2020-10-03 02:31:07 +0000

    Small changes for pypi

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
-from distutils.core import setup
-
+from setuptools import setup, find_packages
+import gym_epidemic
 setup(
   name = 'gym_epidemic',        
   packages = ['gym_epidemic'],   
-  version = '0.0.3',
+  version = '0.0.2b',
   license='BSD-3',  
   description = 'An OpenAI Gym to benchmark AI Reinforcement Learning algorithms in epidemic control problems',  
   author = 'Marcus Lapeyrolerie',                 
   author_email = 'marcuslapeyrolerie@me.com',      
   url = 'https://github.com/boettiger-lab/gym_epidemic',   
-  download_url = 'https://github.com/boettiger-lab/gym_epidemic/releases/tag/v0.0.3',
+  #download_url = 'https://github.com/boettiger-lab/gym_epidemic/releases/tag/v0.0.3',
   keywords = ['Reinforcement Learning', 'Epidemic Control', 'Epidemics', 
               "COVID-19", "AI", "stable-baselines", "OpenAI Gym", 
               "Artificial Intelligence", "Epidemiology"],
